acbf12eb0e8d4ffbf499134ba924790df56b4277
Cuberates <dnxv2409@gmail.com>
2022-09-02 18:00:56 -0500

    updated template

--- CFTEMPLATE.cpp
+++ CFTEMPLATE.cpp
@@ -1,36 +1,47 @@
 #include <bits/stdc++.h>
 
-#define ll long long
-#define debug(x) cout << #x << ": "; _print(x); cout << endl;
-
 using namespace std;
 
-void _print(int x){ cout << x; }
-void _print(ll x){ cout << x; }
-void _print(double x){ cout << setprecision(15) << x;}
-void _print(string x){ cout << x; }
-void _print(char x){ cout << x; }
-void _print(vector<int> x){ for(int i=0;i<x.size();i++){ cout << x[i] << " "; } }
-void _print(vector<char> x){ for(char i : x){ cout << i << " "; } }
-void _print(vector<string> x){ for(string i : x){ cout << i << " "; } }
-void _print(vector<vector<int>> x) { for(int i=0;i<x.size();i++) { cout << "[" << i+1 << "] "; cout << "[ ";for(int j=0;j<x[i].size();j++) cout << x[i][j] << " "; cout << "] \n";} }
-void _print(map<int,int> x) { for(auto item : x) cout << item.first << " " << item.second << "\n";}
-int palindrome(string s) { string p = s; reverse(p.begin(), p.end()); if(s==p) return 1; else return 0;}
-vector<vector<int>> perm(vector<int> x) { vector<vector<int>> permlist; do { permlist.push_back(x); } while (next_permutation(x.begin(), x.end())); return permlist; }
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
 
-const int MXN = 1e5+69420;
-const double DX = 0.000000001, PI = 3.14159265358979, EULER = 2.718281828459045;
+template<typename T, typename V> 
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> 
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";} void _print() {cerr << "]\n";}
+template <typename T, typename... V> 
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 
-void solution() { // JACK NGUYEN >:DDD
-				 		
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solution() { 
+
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int tt; cin >> tt;
+	int tt = 1; 
+   cin >> tt;
 	for(int i=0;i<tt;i++) {
-		cout << "TEST CASE #" << i << " : \n"; // For debugging purposes
+      #ifdef Cuberates
+		   cout << "-- TEST CASE #" << i << " ----- \n";
+      #endif
 		solution();
 	}
 	return 0;

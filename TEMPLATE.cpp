d042120b9389d45852837fd76ebe2dc49aa194e4
Cuberates <dnxv2409@gmail.com>
2022-11-12 13:10:55 +0700

    Cleaner template for readability

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,19 +1,26 @@
 /* Solution by Jack Nguyen @ $%U%$
- * The solution was created on $%M%$ / $%D%$ / $%Y%$
+ * $%M%$ / $%D%$ / $%Y%$
  * */
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
+void mySolution ();
+
 int main () {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	int tt = 1; 
    cin >> tt; 
-   for(int i=0;i<tt;i++) {
-
-   }
+   for(int i=1;i<=tt;i++) {
+  		mySolution ();
+  	}
 	return 0;
 }
 
+/* ---- SOLUTION GOES HERE ---- */
+
+void mySolution () { 
+
+}
